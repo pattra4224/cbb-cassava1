367d7a1eed262192aae71674c99e9abed4d8ab74
pattra4224 <ppattranit08@gmail.com>
2025-02-13 10:12:00 +0700

    main web

---
+++ tab.js
@@ -0,0 +1,7 @@
+document.getElementById("help-btn").addEventListener("click", function () {
+    document.getElementById("help-tab").style.display = "block";
+});
+
+document.getElementById("close-tab").addEventListener("click", function () {
+    document.getElementById("help-tab").style.display = "none";
+});
